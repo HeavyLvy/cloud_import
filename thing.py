88834225d7f59042ca7c199c1dc9b8374ccee3cf
HeavyLvy <90272914+HeavyLvy@users.noreply.github.com>
2023-12-15 18:47:24 -0700

    Update thing.py

--- thing.py
+++ thing.py
@@ -1,3 +1,6 @@
+"""
+Just a simple test, tried using test.py but test already exists as a file in python so i just decided to make a new one called thing.py
+"""
 import fruits
 
 apple = fruits.Fruit('apple')
